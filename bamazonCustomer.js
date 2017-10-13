038ec83919a1615aa441473d7807fc7b1ca3b2c4
Live Sound Systems <hillaryellis37@gmail.com>
2017-10-12 20:00:49 -0700

    added node item prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var itemID;
+var itemQuantity;
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -14,7 +17,50 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  // createProduct();
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId + "\n");
+	readProducts();
 });
+
+function readProducts() {
+	console.log("Displaying all products...\n");
+	connection.query("SELECT * FROM products", function(err, res) {
+	    if (err) throw err;
+	    for (var i = 0; i < res.length; i++) {
+	    	console.log("Item ID:", res[i].id, 
+	    				"\nName:", res[i].product_name,
+	    				"\nPrice:", "$" + res[i].price,
+	    				"\n");
+	    }
+	    promptItem();
+	    connection.end();
+
+	});
+}
+
+function promptItem() {
+	inquirer
+	  .prompt([
+	    {
+	      type: "input",
+	      message: "Please enter item the 'ID' to purchase: ",
+	      name: "itemID"
+	    },
+	   	{
+	      type: "input",
+	      message: "How many units would you like to purchase?",
+	      name: "quantity"
+	    }
+	  ])
+	  .then(function(inquirerResponse) {
+	  	// console.log(inquirerResponse.itemID);
+	  	// console.log(inquirerResponse.quantity);
+	  	itemID = inquirerResponse.itemID;
+	  	itemQuantity = inquirerResponse.quantity;
+
+	  	console.log(itemID);
+	  	console.log(itemQuantity);
+
+
+	  });
+}
